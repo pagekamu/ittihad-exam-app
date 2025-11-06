c99570fc43a1d0a6ab52f8120231428b10afc1a3
Nabila Nailatus S. <104802051+pagekamu@users.noreply.github.com>
2025-11-06 15:42:42 +0700

    Create firebase-config.js

---
+++ js/firebase-config.js
@@ -0,0 +1,11 @@
+// Firebase Configuration
+// You can replace these values with your own Firebase project config if needed.
+
+const FIREBASE_CONFIG = {
+  apiKey: "AIzaSyDT8Rgg8Rt-6jmA6wg0hJJOOhsKOWNqD-U",
+  authDomain: "exam-app-f9bce.firebaseapp.com",
+  projectId: "exam-app-f9bce",
+  storageBucket: "exam-app-f9bce.firebasestorage.app",
+  messagingSenderId: "818751284037",
+  appId: "1:818751284037:web:4e519c3e7dd16196b0142c"
+};
