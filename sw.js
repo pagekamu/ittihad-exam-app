9169a2d4ed40c1b73ac0765a1b4b2f804323b340
Nabila Nailatus S. <104802051+pagekamu@users.noreply.github.com>
2025-11-06 15:45:15 +0700

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,15 @@
+{
+  "name": "Ittihadul Exam",
+  "short_name": "ExamApp",
+  "start_url": "./index.html",
+  "display": "standalone",
+  "background_color": "#e9f5e9",
+  "theme_color": "#4CAF50",
+  "icons": [
+    {
+      "src": "https://i.imgur.com/3HOXdjB.png",
+      "sizes": "192x192",
+      "type": "image/png"
+    }
+  ]
+}
